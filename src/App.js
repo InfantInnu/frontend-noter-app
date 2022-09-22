f517168944da67e68871508e9921d27eb72db4f2
infant <y.infant1996@gmail.com>
2022-09-22 14:20:32 +0530

    frontend noter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+import CreatePage from "./pages/CreatePage";
+import DashboardPage from "./pages/DashboardPage";
+import DeletePage from "./pages/DeletePage";
+import SigninPage from "./pages/SigninPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const token = localStorage.getItem("token");
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<SigninPage />} />
+                    <Route path="/dashboard" element={<DashboardPage />} />
+                    <Route path="/create" element={<CreatePage />} />
+
+                    <Route path="/deletetask/:id" element={<DeletePage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
